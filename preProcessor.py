f805bf919ecf41ac7f9327af893b9e32fa5414bc
Abenezer <ayonathan0@gmail.com>
2024-05-26 21:33:56 +0300

    s

--- preProcessor.py
+++ preProcessor.py
@@ -84,7 +84,7 @@ demo_selected = st.sidebar.radio(label="Select Video Type", options=["Preprocess
 parent_directory = '/Users/abenezer/Documents/Projects/SOKA/video_analyzer/projects'
 
 if demo_selected == 'Preprocess':
-    calibration_data = np.load('/Users/abenezer/Documents/Projects/SOKA/video_analyzer/working_frames/wide_angle_calib_02/calib_matrix_02.npz')
+    # calibration_data = np.load('/Users/abenezer/Documents/Projects/SOKA/video_analyzer/working_frames/wide_angle_calib_02/calib_matrix_02.npz')
    
     input_vide_file, tempf, video_meta_0, df = video_uploader('new')
     
@@ -93,7 +93,7 @@ if demo_selected == 'Preprocess':
         preprocessor(project_folder)
     
 elif demo_selected == 'Analyze':
-    calibration_data = np.load('/Users/abenezer/Documents/Projects/SOKA/video_analyzer/functions/sony_4k_calibration_data.npz')
+    calibration_data = np.load('functions/sony_4k_calibration_data.npz')
     input_vide_file, tempf, video_meta_0, df = video_uploader('new_uu')
     
     if input_vide_file:
